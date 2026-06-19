49023f33d23493e2fef97f5ffcff395915e686a5
tanzia mourin <tanziamourin68@gmail.com>
2026-06-19 20:50:01 +0600

    updeated suth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,8 +2,8 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: "https://food-hub-backend-one.vercel.app",
-   cookies: {
-    secure: true,
-    sameSite: "none",
-  },
+  //  cookies: {
+  //   secure: true,
+  //   sameSite: "none",
+  // },
 });
